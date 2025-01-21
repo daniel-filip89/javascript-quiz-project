50d98fd467863e783fed3ed6812f6e3969002006
John Doe <johndoe@example.com>
2025-01-21 16:56:57 +0100

    Solved Lab Day2 Seriog & Daniel

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,56 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    // Constructor
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    // Get current question
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
 
-    // 4. shuffleQuestions()
+    // Move to next question
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
 
-    // 5. checkAnswer(answer)
+    // Shuffle the questions
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
 
-    // 6. hasEnded()
+    // Check if the answer is correct
+    checkAnswer(answer) {
+        if (answer === this.questions[this.currentQuestionIndex].answer) {
+            this.correctAnswers++;
+        }
+    }
+
+    // Check if the quiz has ended
+    hasEnded() {
+        return this.currentQuestionIndex >= this.questions.length;
+    }
+
+    // Filter questions by difficulty
+    filterQuestionsByDifficulty(difficulty) {
+        // Check if difficulty is a valid number between 1 and 3
+        if (difficulty >= 1 && difficulty <= 3) {
+            this.questions = this.questions.filter(question => question.difficulty === difficulty);
+        }
+    }
+
+    // Calculate the average difficulty of all questions
+    averageDifficulty() {
+        const totalDifficulty = this.questions.reduce((sum, question) => sum + question.difficulty, 0);
+        return totalDifficulty / this.questions.length;
+    }
 }
+
+console.log(Hello);
